30400c5152da5470db3f10a0012874a220cd1ae1
JavelFreitas <javelqueiroz@outlook.com>
2018-10-31 00:50:04 -0300

    Nao deu certo

--- Repositorio/teste.cpp
+++ Repositorio/teste.cpp
@@ -1,14 +1,202 @@
 #include <iostream>
-#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <sstream> 
 using namespace std;
 
+class Student{
+    string name, program, registration;
+public:
+
+    Student(string registration = "", string program = "", string name = ""){
+        this->registration = registration;
+        this->program = program;
+        this->name = name;
+    }
+
+    string toString(){
+        stringstream out;
+        out << registration << ":" << program << ":" << name;
+        return out.str();
+    }
+
+};
+
+class Teacher{
+    string name;
+    string registration;
+public:
+
+    Teacher(string registration = "", string name = ""){
+        this->registration = registration;
+        this->name = name;
+    }
+
+    string toString(){
+        stringstream out;
+        out << registration << ":" << name;
+        return out.str();
+    }
+};
+
+class Subject{
+    string id, name;
+public:
+
+    Subject(string id = "", string name = ""){
+        this->id = id;
+        this->name = name;
+    }
+
+    string toString(){
+        stringstream out;
+        out << id << ":" << name;
+        return out.str();
+    }
+    
+};
+
+template<class T>
+struct Repository{
+    map<string, T> all;
+
+    void add(string key, T thing){
+        if(!exist(key)){
+            all[key] = thing;
+        }else{
+            throw "Key already exists, try another";
+        }
+    }
+
+    bool exist(string key){
+        return all.find(key) != all.end();
+    }
+
+    void rm(string key){
+        if(!exist(key)){
+            throw "Key does not exist";
+        }else{
+            all.erase(key);
+        }
+    }
+
+    vector<T> getValues(){
+        vector<T> vt;
+        for(auto& par : all){
+            vt.push_back(par.second);
+        }
+        return vt;
+    }
+
+    vector<string> getKeys(){
+        vector<string> vt;
+        for(auto& par : all){
+            vt.push_back(par.first);
+        }
+        return vt;
+    }
+
+    T& getV(string key){
+        auto it = all.find(key);
+        if (it != all.end()){
+            return it->second;
+        }
+        throw "Key not found";
+    }
+
+    string toString(vector<T> vet){
+        stringstream out;
+        for(auto& thing : vet){
+            out << thing.toString() << "\n";
+        }
+        return out.str();
+    }
+
+};
+
+struct Controller{
+
+    Repository<Student> rstu;
+    Repository<Teacher> rtea;
+    Repository<Subject> rsub;
+
+//This code can still be shortened
+
+    void shell(string line){
+        stringstream in(line);
+        string op, newKey;
+        in >> op;
+        if(op == "addAlu" || op == "addPro" || op == "addDis" ){
+            string newReg, newProg, newName;
+            if(op == "addAlu"){
+                in >> newReg >> newProg;
+                getline(in, newName);
+                newName = newName.substr(1);
+                rstu.add(newReg, Student(newReg, newProg, newName));
+            }else if(op == "addProf"){
+                in >> newReg;
+                getline(in, newName);
+                newName = newName.substr(1);
+                rtea.add(newReg, Teacher(newReg, newName));
+            }else{
+                in >> newReg;
+                getline(in, newName);
+                newName = newName.substr(1);
+                rsub.add(newReg, Subject(newReg, newName));
+            }
+        }else if(op == "showAlu" || op == "showPro" || op == "showDis"){
+            if(op == "showAlu")
+                cout << rstu.toString(rstu.getValues());
+            else if(op == "showPro")
+                cout << rtea.toString(rtea.getValues());
+            else
+                cout << rsub.toString(rsub.getValues());
+        }else if(op == "rmAlu"|| op == "rmPro" || op == "rmDis"){
+            in >> newKey;
+            if(op == "rmAlu"){
+                rstu.rm(newKey);
+            }else if(op == "rmPro"){
+                rtea.rm(newKey);
+            }else{
+                rsub.rm(newKey);
+            }
+        }else if(op == "getAlu"|| op == "getPro" || op == "getDis"){
+            in >> newKey;
+            if(op == "getAlu"){
+                Student& stu = rstu.getV(newKey);
+                cout << stu.toString() << endl; 
+            }else if(op == "getPro"){
+                Teacher& tea = rtea.getV(newKey);
+                cout << tea.toString() << endl;
+            }else{
+                Subject& sub = rsub.getV(newKey);
+                cout << sub.toString() << endl;
+            }
+        }else{
+            throw "Command do not exist";
+        }
+        throw "^Done";
+    }
+
+    void exec(){
+        string line;
+        while(true){
+            getline(cin, line);
+            if(line == "end"){
+                return;
+            }else{
+                try{
+                    shell(line);
+                }catch(const char * warning){
+                    cout << warning << endl;
+                }
+            }
+        }
+    }
+};
 int main(){
-    string line;
-    getline(cin, line);
-    stringstream in(line);
-    int number;
-    string name, object;
-    in >> number >> name >> object;
-    cout << number << name << object;
+    Controller t;
+    t.exec();
     return 0;
 }
